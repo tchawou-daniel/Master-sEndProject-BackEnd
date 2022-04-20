23cf76fc7c06ee529ff31f73f6319a72236478f1
daniel-sipe <daniel@amalia.io>
2022-04-20 22:51:29 +0200

    feat(#14-build-basic-entities): :construction:

---
+++ apps/api/src/usersWorkForCompanies/dto/create-usersWorkForCompanies.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateUsersWorkForCompaniesDto {
+  @IsOptional()
+  @IsNumber()
+  scoreCompany?: number;
+
+  @IsOptional()
+  @IsString()
+  companyReviews?: string;
+
+  @IsOptional()
+  @IsNumber()
+  workerReviews?: number;
+}
---
+++ apps/api/src/usersWorkForCompanies/dto/update-usersWorkForCompanies.dto.ts
@@ -0,0 +1,15 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUsersWorkForCompaniesDto {
+  @IsOptional()
+  @IsNumber()
+  scoreCompany?: number;
+
+  @IsOptional()
+  @IsString()
+  companyReviews?: string;
+
+  @IsOptional()
+  @IsNumber()
+  workerReviews?: number;
+}
